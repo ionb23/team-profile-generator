264a845c96db94611851b6761f4820e91eaaaa79
ionb23 <ion.borziac@gmail.com>
2023-02-26 09:13:53 +0000

    added questions for all roles in index.js

--- index.js
+++ index.js
@@ -12,4 +12,116 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Pseudo code from Dan Mueller (instructor) from class
 
+inquirer.prompt([
+    //manager questions
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the team manager's name?",
+    },
+    {
+        type: 'number',
+        name: 'id',
+        message: "What is the team manager's id?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the team manager's email?",
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the team manager's office number?",
+    },
+]).then(response => {
+    // populate manager info
+    // promptForNextEmployee ()
+})
+
+const promptForNextEmployee = () => {
+    inquirer.prompt([
+        // choice of 3
+        {
+            type: 'list',
+            name: 'newTeamMemberType',
+            message: "Which type of team member would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I don't want to add any more team members",
+            ],
+        },
+    ]).then(response => {
+        // if engineer
+        //    promptForEngineer
+        // else if intern
+        //    promptForIntern
+        // else
+        //    use the functionality from page-template to generate the team
+    })
+}
+
+const promptForEngineer = () => {
+    inquirer.prompt([
+        //engineer questions
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your engineer's name?",
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: "What is your engineer's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your engineer's email?",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is your engineer's Github username?",
+        },
+    ]).then(response => {
+        // add new engineer to employees array
+        // promptForNextEmployee
+    })
+}
+
+const promptForIntern = () => {
+    inquirer.prompt([
+        //intern questions
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your intern's name?",
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: "What is your intern's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your intern's email?",
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is your intern's school?",
+        },
+    ]).then(response => {
+        // add new intern to employees array
+        // promptForNextEmployee
+    })
+}
+
+const buildPage = () => {
+    // render(myArrayOfTeamMembers)
+}
